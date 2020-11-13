35ecdbf3b2d9676f483a006286c7752296125ce5
007steve <stephen.plummer1.sp@gmail.com>
2020-11-13 00:08:13 -0500

    completed

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/MessageCard.css";
 import { motion } from "framer-motion";
-function MessageCard() {
+function MessageCard({ image, texts, time, notification, name }) {
   return (
     <motion.div
       drag
@@ -9,23 +9,17 @@ function MessageCard() {
       dragElastic={0.2}
       className="messageCard"
     >
-      <div className="messageCard-image-container">
-        <img
-          className="messageCard-image"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAnkVD95W4n3EcRVI6dsAPjDix0nJSWs5xdw&usqp=CAU"
-          alt
-        />
-      </div>
+      <img className="messageCard-image" src={image} alt />
 
       <div className="messageCard-info-container">
-        <h4 className="messageCard-h4">Name</h4>
-        <p className="messageCard-content">Text content goes here whats up!</p>
+        <h4 className="messageCard-h4">{name}</h4>
+        <p className="messageCard-content">{texts}</p>
       </div>
       <div className="messageCard-notificaton-container">
         <div className="messageCard-notificaton">
-          <span>1</span>
+          <span>{notification}</span>
         </div>
-        <p className="messageCard-time">2M</p>
+        <p className="messageCard-time">{time}</p>
       </div>
     </motion.div>
   );
