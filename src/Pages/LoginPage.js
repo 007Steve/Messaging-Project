35720fb66193efb2a7aac65adc5ee5df865fcfed
007steve <stephen.plummer1.sp@gmail.com>
2021-01-04 20:48:11 -0500

    added demo login

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "../styles/LoginPage.css";
-import { Button } from '@material-ui/core'
-import { auth, provider } from '../firebase'
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 function LoginPage() {
-    const signIn = () => {
-    auth.signInWithPopup(provider).catch((error) => <h2>{error.message}</h2>)
-    }
+  const dispatch = useDispatch();
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => <h2>{error.message}</h2>);
+  };
 
-  
+  const demo = () => {
+    dispatch(login({}));
+  };
   return (
     <div className="loginPage">
-          <div className="loginPage__logo">
-                <img src="/Untitled design (7).png"/>
-            </div>
-            <Button className="btn-grad" onClick={signIn}>SIGN IN</Button>
+      <div className="loginPage__logo">
+        <img src="/Untitled design (7).png" alt="logo"/>
+      </div>
+      <Button className="btn-grad" onClick={signIn}>
+        SIGN IN
+      </Button>
+      <Button className="btn-grad" onClick={demo}>
+        Demo
+      </Button>
     </div>
   );
 }
